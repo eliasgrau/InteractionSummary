6b92616bedcfa0ff4d52c8ba0175c206768211a0
Elias <Graue421@newschool.edu>
2020-02-28 10:53:19 -0500

    added js assignment and edited it

---
+++ jslesson/main.js
@@ -0,0 +1,7 @@
+function changeHeading () {
+	document.getElementById('heading').style.backgroundColor="lightblue"
+}
+
+function changeHeading () {
+	document.getElementById('heading').innerHTML="i clicked the button"
+}
